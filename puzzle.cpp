ca29b1ba427ddf06aa72383d6f3ed38226677807
ThiagoFPMR <thiagofpmr@gmail.com>
2019-08-15 10:09:32 -0300

    Goals

--- puzzle.cpp
+++ puzzle.cpp
@@ -1,9 +1,12 @@
 #include <iosteam>
-using namespace std;
+
+// Objetivos:
+// 1. Fazer o criador de navios, que gera o documento no formato da armada.png aleatoriamente a partir do valor de rows e collums e fornece as posição para 2
+// 2. Fazer o gerador de matrizes com base nas posiçẽos geradas pelo 1 e no tamanho especificado
+// 3. Fazer a função ler quantity rows e collums a partir da chamada pelo terminal (olhar slack)
 
 int main(){
-  int quantity;
-
+  int Quantity, Rows, Collums;
 
   return 0;
 }
