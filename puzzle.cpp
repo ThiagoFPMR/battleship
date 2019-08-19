b903c5f98c49a1cf14b915b9f3a9d33aa13d43b2
Jorbralyson Freire <jorbralyson.freire@A21214018525LL.imd.local>
2019-08-19 13:05:23 -0300

    Includes e name space

--- puzzle.cpp
+++ puzzle.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <vector>
+#include <./models/Map.cpp>
 
 // Objetivos:
 // 1. Fazer o criador de navios, que gera o documento no formato da armada.png aleatoriamente a partir do valor de rows e collums e fornece as posição para 2
 // 2. Fazer o gerador de matrizes com base nas posiçẽos geradas pelo 1 e no tamanho especificado
 // 3. Fazer a função ler quantity rows e collums a partir da chamada pelo terminal (olhar slack)
 
+using namespace std;
 int main(){
   int Quantity, Rows, Collums;
+  vector <vector <int> > armada;
   //inputs
   //fazer o front
   return 0;
