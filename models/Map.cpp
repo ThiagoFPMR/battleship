0f1f359f0f7c7845d7155b945a30b0e25ccaa5dc
shadowpl4ys@gmail.com <shadowpl4ys@gmail.com>
2019-08-22 19:10:49 -0300

    replace

--- models/Map.cpp
+++ models/Map.cpp
@@ -1,7 +1,8 @@
-using namespace std;
+#include <iostream>
+
 class Map{
     public:
-    Map(int Rows, int Collums){
+    Map(){
       /* Plano de Ataque:
          1. Decidir o tipo de barco a ser posicionado
          2. Decidir se é horiozontal ou vertical
@@ -10,43 +11,57 @@ class Map{
          4. Posicionar o barco selecionado
          5. Repetir para os 10 barcos
       */
-        //valor de cada ship[x] será o tamanho da embarcação x
-        vector<int> ships = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
-        int board[Rows][Collums] = {{0}};
-        
-        
-        for(int k = 0; k <= ships.size(); k++){
-          int r = rand() % Rows;
-          int c = rand()% Collums;
-          board[r][c] = 1;
-          bool rotation = rand() % 2;
-          
-          if(rotation == 0)
-            for(int s = r; s <= ships[k]; s++)
-              board[s][c] = 1;
-          if(rotation == 1)
-            for(int s = c; s <= ships[k]; s++)
-              board[r][s] = 1;
-        }
+        // int ships[4] = {1, 2, 3, 4};
+       Board[Rows][Collums] = {{0}};
+       int random;
 
-        // Posicionar o Battleship
+       // Posicionar o Battleship
+
+       // Definir Direção
+       int directions[2] = {'H', 'V'};
+       int direction = directions[rand() % 2];
+
+       // Escolher as cordenadas m (linha) e n (coluna)
+       m = rand() % (Rows - 4);
+       n = rand() % (Collums - 4);
+
+       int i = m - 1, i_max = m + 1, e = n - 1, e_max = n + 4;
+       if (i < 0){
+         i = m;
+       }
+       if (i_max >= Rows){
+         i_max = m;
+       }
+       if (e < 0){
+         e = n;
+       }
+       if (e_max >= Collums){
+         e_max = n + 3;
+       }
+
+       for (i; i <= i_max; i++){
+         for (e; e <= e_max; e++){
+           Board[i][e] = 1;
+         }
+       }
 
 
-        // Definir Direção
-        int directions[2] = {'H', 'V'};
-        int direction = directions[rand() % 2];
 
-        // Escolher as cordenadas m (linha) e n (coluna)
-        int m = rand() % (Rows - 4);
-        int n = rand() % (Collums - 4);
 
-        //Preenchendo o espaço ocupado pelo vetor
-        /*if (m > 0 && m < Rows - 1  && n > 0 && n < Collums -1){
-          for (int i = m - 1; i < m + 4; i++){
-            for (int e = n -1; i < n + 2; e++){
-              board[i][e] = 1;
-            }
-          }
-        } */
+
+
+       //Preenchendo o espaço ocupado pelo vetor
+       if (m > 0 && m < Rows - 1  && n > 0 && n < Collums -1){
+         for (int i = m - 1; i < m + 4; i++){
+           for (int e = n -1; i < n + 2; e++){
+             Board[i][e] = 1;
+           }
+         }
+       }
+
+
+
+
+
     }
 };
