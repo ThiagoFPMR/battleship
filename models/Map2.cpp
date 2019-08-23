a0885ee2f95cb606d2fadb2d655bbde75a1da780
JorbFreire <jorbralyson.freire@A21214018525LL.imd.local>
2019-08-23 19:40:44 -0300

    legacy change name

--- models/Map2.cpp
+++ models/Map2.cpp
@@ -2,6 +2,65 @@
 using namespace std;
 class Map{
     public:
+    void add_ship(int Rows, int Collums, int lenght){
+        // Obs: largura do barco (lenght) será argumento para a função
+
+        // Definir Direção
+        int directions[2] = {'H', 'V'};
+        int direction = directions[rand() % 2];
+
+        // Escolher as cordenadas m (linha) e n (coluna)
+        int m = rand() % (Rows - lenght);
+        int n = rand() % (Collums - lenght);
+
+        // Posicionamento
+        switch(direction){
+          case 'H': 
+            int i = m - 1,j 
+            i_max = n + 1, 
+            e = n - 1, 
+            e_max = m + lenght; 
+            break;
+          case 'V': 
+            int i = m - 1, 
+            i_max = m + lenght, 
+            e = n - 1, 
+            e_max = n + 1; 
+            break;
+        }
+
+        if (i < 0)
+          i = m;
+
+        if (i_max >= Rows){
+          switch(direction){
+            case 'H': 
+              i_max = m; 
+              break;
+            case 'V': 
+              i_max = m + lenght - 1; 
+              break;
+          }
+        }
+        if (e < 0)
+          e = n;
+
+        if (e_max >= Collums){
+          switch(direction){
+            case 'H': 
+              e_max = n + lenght - 1;
+              break;
+            case 'V': 
+              e_max = n;
+              break;
+          }
+        }
+
+        for (i; i <= i_max; i++)
+          for (e; e <= e_max; e++)
+            board[i][e] = 1;
+            
+      }
     Map(int Rows, int Collums){
       /* Plano de Ataque:
          1. Decidir o tipo de barco a ser posicionado
@@ -11,7 +70,7 @@ class Map{
          4. Posicionar o barco selecionado
          5. Repetir para os 10 barcos
       */
-       vector<int> ships = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
+       /*vector<int> ships = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
        char board[Rows][Collums] = {{0}};
 
       for(int k = 0; k <= ships.size(); k++){
@@ -63,65 +122,11 @@ class Map{
         default:
           break;
         }
-      }
+      }*/
        // É melhor criar uma função universal e usa-la para todos os barcos
-
-      /*void add_ship(Rows, Collums, lenght){
-        // Obs: largura do barco (lenght) será argumento para a função
-
-        // Definir Direção
-        int directions[2] = {'H', 'V'};
-        int direction = directions[rand() % 2];
-
-        // Escolher as cordenadas m (linha) e n (coluna)
-        int m = rand() % (Rows - lenght;
-        int n = rand() % (Collums - lenght);
-
-        // Posicionamento
-        switch(direction){
-          case 'H': 
-            int i = m - 1, 
-            i_max = n + 1, 
-            e = n - 1, 
-            e_max = m + lenght; 
-            break;
-          case 'V': 
-            int i = m - 1, 
-            i_max = m + lenght, 
-            e = n - 1, 
-            e_max = n + 1; 
-            break;
-        }
-
-        if (i < 0){i = m;}
-
-        if (i_max >= Rows){
-          switch(direction){
-            case 'H': i_max = m; break;
-            case 'V': i_max = m + lenght - 1; break;
-          }
-        }
-        if (e < 0){e = n;}
-
-        if (e_max >= Collums){
-          switch(direction){
-            case 'H': e_max = n + lenght - 1; break;
-            case 'V': e_max = n; break;
-          }
-        }
-
-        for (i; i <= i_max; i++){
-          for (e; e <= e_max; e++){
-            board[i][e] = 1;
-          }
-        }
-      }
-
       // Posicionar o Battleship
 
-      add_ship(Rows, Collums, 4); */
-
-
+      add_ship(Rows, Collums, 4);
 
     }
 };
