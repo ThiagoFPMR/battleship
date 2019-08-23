a65adef40899b8516a9de92d87e2462402ae645c
JorbFreire <jorbralyson.freire@A21214018525LL.imd.local>
2019-08-23 19:41:36 -0300

    tentar fazer um print

--- puzzle.cpp
+++ puzzle.cpp
@@ -13,15 +13,18 @@ int main(int argc, char *argv[]){
   vector <Map*> matBool; 
 
   //arguemntos na ordem
-  quantity = atoi(argv[1]);
-  rows = atoi(argv[2]);
-  collums = atoi(argv[3]);
+  quantity = atoi(argv[0]);
+  rows = 10;//atoi(argv[1]);
+  collums = 10;//atoi(argv[2]);
 
   srand(time(NULL));
   for(int k = 0; k < quantity; k++){
     matBool.push_back(new Map(rows, collums));
     cout << quantity << endl;
-  } 
+    for(int w = 0; w <= matBool[k]->board.size(); w++)
+      for(int j = 0; j <= matBool[k]->board[w].size(); j++)
+        cout << matBool[k]->board[w][j];
+  }
 
   //fazer o front
   return 0;
