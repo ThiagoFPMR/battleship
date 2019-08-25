99dd02c7128116c9f781febdb062b2200c7a5275
JorbFreire <jorbfreire@ufrn.edu.br>
2019-08-25 14:16:18 -0300

    3d vector template

--- tests vector/main.cpp
+++ tests vector/main.cpp
@@ -3,16 +3,27 @@
 
 using namespace std;
 int main(){
-    vector <vector <int>> libvec;
+    vector <int> dim1;
+    vector <vector<int>> dim2;
+    vector <vector <vector <int>>> vec;
 
     for(int k = 0; k <= 3; k++)
-        libvec.push_back({2, 2, 2, 2});
-        //for(int kk = 0; kk <= 3; kk++)
-            //libvec[k].push_back(2);
+        dim1.push_back(2);
 
-    for(int k = 0; k < libvec.size(); k++){
-        for(int kk = 0; kk < libvec.size(); kk++)
-            cout << libvec[k][kk] << "  ";
-        cout << endl;
+    for(int k = 0; k <= 3; k++)
+        dim2.push_back(dim1);
+
+    for(int k = 0; k <= 3; k++)
+        vec.push_back(dim2);
+
+    
+    cout << "vec = ";
+    for(int j = 0; j < vec.size(); j++){
+        for(int k = 0; k < vec[j].size(); k++){
+            cout << endl;
+            for(int k2 = 0; k2 < vec[j][k].size(); k2++)
+                cout << dim2[k][k2] << ", ";
+        }
+    cout << "\n\n\n";
     }
 }
