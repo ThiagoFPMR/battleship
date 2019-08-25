6f6fc3e7dbb11cb5332e7dda42cd9a2688fc3b75
JorbFreire <jorbfreire@ufrn.edu.br>
2019-08-25 20:19:31 -0300

    sintaxe corrigida (parcialmente)

--- models/Map3.cpp
+++ models/Map3.cpp
@@ -1,14 +1,16 @@
+#include <vector>
+
 using namespace std;
 
 class Map{
 public:
     Map(int Rows, int Collums){
 
-        int ships[10] = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
+        vector <int> ships = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
         int occupied_m[120]; // Pior caso o numero de casas ocupadas é 120
         int occupied_n[120]; // Então podemos definir isso como tamanho maximo do vetor
 
-        for (int k = 0; k < ships.size(); K++){
+        for (int k = 0; k < ships.size(); k++){
 
             char type;
             switch(ships[k]){ // Definir o tipo de barco a ser posicionado
@@ -16,13 +18,13 @@ public:
                     type = 'S';
                     break;
                 case 2:
-                    type = 'C'
+                    type = 'C';
                     break;
-                case 3;
-                    type = 'D'
+                case 3:
+                    type = 'D';
                     break;
                 case 4:
-                    type = 'B'
+                    type = 'B';
                     break;
             }
 
@@ -36,8 +38,8 @@ public:
             // Redefinir m e n para o caso das coordenadas escolhidas estiverem ocupadas
             for (int w = 0; w < occupied_m.size(); w++){
                 if (m == occupied_m[w] && n == occupied_n[w]){
-                    m = rand() % (Rows - ships[k])
-                    n = rand() % (Collums - ships[k])
+                    m = rand() % (Rows - ships[k]);
+                    n = rand() % (Collums - ships[k]);
                 }
             }
 
