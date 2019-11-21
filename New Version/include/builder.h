5d4e49dc459a9894ad4180d8eac43fc8360f468e
ThiagoFPMR <shadowpl4ys@gmail.com>
2019-11-21 18:05:55 -0300

    build.h update

--- New Version/include/builder.h
+++ New Version/include/builder.h
@@ -10,5 +10,7 @@
 
 void boardBuilder(Board map, int rows, int collums);
 void setup (Board map); // Inicializa o mapa, preenchendo todas as posições
+bool canPlaceBoat(Board map, int row, int collum, int direction, int boatSize); // Checa se é possivel colocar um barco na posição
+void setBoat(Board map, int boatSize); // Coloca o barco na posição
 
 #endif
