36b42f017125db60ee2176df368ad3ab696b9457
JorbFreire <jorbfreire@ufrn.edu.br>
2019-11-21 21:33:25 -0300

    pointers

--- New Version/src/builder.cpp
+++ New Version/src/builder.cpp
@@ -3,12 +3,11 @@
 using namespace std;
 
 void boardBuilder (Board map, int rows, int collums) {
-    vector <int> boats = {1, 1, 1, 1, 2, 2, 2, 3, 3, 4};
+    vector <unsigned short int> boats = {1, 1, 1, 1, 2, 2, 2, 3, 3, 4};
     Board map(rows, collums);
     std::ostringstream OSS;
 
-
-
+    //top da armada
     OSS << "Linhas: " << rows 
         << "Colunas: " << collums 
         << "\n\n";
@@ -18,8 +17,9 @@ void boardBuilder (Board map, int rows, int collums) {
     //Inicializa a matriz
     setup(map);
 
-    for (int i = boats.back(); i >= 0; i++) { 
-        setBoat(map, boats[i]);
+    for (int i = boats.back(); i >= 0; i++) {
+        unsigned short int* boat = &boats[i];
+        setBoat(map, *boat);
         boats.pop_back();
     }
     /*Lembrar para quando for implementar o resto:
@@ -35,7 +35,7 @@ void setup (Board map) {
             map.matrix[m][n] = ".";
 }
 
-void setBoat (Board map, int boatSize) {
+void setBoat (Board map, unsigned short int *boatSize) {
     random_device r_col, r_row, r_direction;
     /* Começa definindo 3 números aleatorios acima,
      os quais usados como base para obter a posição
@@ -45,16 +45,16 @@ void setBoat (Board map, int boatSize) {
     int direction = r_direction() % 2;
     // 0 -> Horizontal | 1 -> Vertical
 
-    if (!(canPlaceBoat(map, row, collum, direction, boatSize))) {
-        setBoat(map, boatSize);
+    if (!(canPlaceBoat(map, row, collum, direction, *boatSize))) {
+        setBoat(map, *boatSize);
     }
     else {
         // Adicionando o barco à armada
         std::ostringstream OSS;
 
-        OSS << boatSize << " "
-            << row      << " " 
-            << collum   << " " 
+        OSS << *boatSize << " "
+            << row       << " " 
+            << collum    << " " 
             << direction << "\n";
         map.fleet+= OSS.str();
 
@@ -62,15 +62,15 @@ void setBoat (Board map, int boatSize) {
 
         if (direction == 0) { // Se for horizontal
             // Posiciona o barco
-            for (int n = collum; n < collum + boatSize; n++)
+            for (int n = collum; n < collum + *boatSize; n++)
                 map.matrix[row][n] = "◼︎";
             // Ocupa posições acima do barco
             if (row > 0)
-                for (int n = collum; n < collum + boatSize; n++)
+                for (int n = collum; n < collum + *boatSize; n++)
                     map.matrix[row - 1][n] = "*";
             // Ocupa posições abaixo do barco
             if (row < map.rows() - 1)
-                for (int n = collum; n < collum + boatSize; n++)
+                for (int n = collum; n < collum + *boatSize; n++)
                     map.matrix[row + 1][n] = "*";
             // Ocupa posições à direita e à esquerda (e diagonais)
             if (collum > 0) {
@@ -80,25 +80,25 @@ void setBoat (Board map, int boatSize) {
                 if (row < map.rows() - 1)
                     map.matrix[row + 1][collum - 1] = "*";
             }
-            if (collum < map.collums() - boatSize) {
-                map.matrix[row][collum + boatSize] = "*";
+            if (collum < map.collums() - *boatSize) {
+                map.matrix[row][collum + *boatSize] = "*";
                 if (row > 0)
-                    map.matrix[row - 1][collum + boatSize] = "*";
+                    map.matrix[row - 1][collum + *boatSize] = "*";
                 if (row < map.rows() - 1)
-                    map.matrix[row + 1][collum + boatSize] = "*";
+                    map.matrix[row + 1][collum + *boatSize] = "*";
             }
         }
         if (direction == 1) { // Se for vertical
             // Posiciona o barco
-            for (int m = row; m < row + boatSize; m++)
+            for (int m = row; m < row + *boatSize; m++)
                 map.matrix[m][collum] = "◼︎";
             // Ocupa poisções à esqueda do barco
             if (collum > 0)
-                for (int m = row; m < row + boatSize; m++)
+                for (int m = row; m < row + *boatSize; m++)
                     map.matrix[m][collum - 1] = "*";
             // Ocupa posições à direita do barco
             if (collum < map.collums() - 1)
-                for (int m = row; m < row + boatSize; m++)
+                for (int m = row; m < row + *boatSize; m++)
                     map.matrix[m][collum + 1] = "*";
             // Ocupa posições abaixo e acima (e diagonais)
             if (row > 0) { 
@@ -108,32 +108,32 @@ void setBoat (Board map, int boatSize) {
                 if (collum < map.collums() - 1)
                     map.matrix[row - 1][collum + 1];
             }
-            if (row < map.rows() - boatSize)
-                map.matrix[row + boatSize][collum] = "*";
+            if (row < map.rows() - *boatSize)
+                map.matrix[row + *boatSize][collum] = "*";
                 if (collum > 0)
-                    map.matrix[row + boatSize][collum - 1] = "*";
+                    map.matrix[row + *boatSize][collum - 1] = "*";
                 if (collum < map.collums() - 1)
-                    map.matrix[row + boatSize][collum + 1];
+                    map.matrix[row + *boatSize][collum + 1];
         }
     }
 }
 
-bool canPlaceBoat (Board map, int row, int collum, int direction, int boatSize) {
+bool canPlaceBoat (Board map, int row, int collum, int direction, unsigned short int *boatSize) {
     if (direction == 0) { // Se for horizontal
         if (!(row < map.rows() && row >= 0))
             return false;
-        if (!(collum < map.collums() - boatSize && collum >= 0))
+        if (!(collum < map.collums() - *boatSize && collum >= 0))
             return false;
-        for (int n = collum; n < collum + boatSize; n++)
+        for (int n = collum; n < collum + *boatSize; n++)
             if (map.matrix[row][n] != ".")
                 return false;
     }
     if (direction == 1) { // Se for vertical
-        if (!(row < map.rows() - boatSize && row >= 0))
+        if (!(row < map.rows() - *boatSize && row >= 0))
             return false;
         if (!(collum < map.collums() && collum >= 0))
             return false;
-        for (int m = row; m < row + boatSize; m++)
+        for (int m = row; m < row + *boatSize; m++)
             if (map.matrix[m][collum] != ".")
                 return false;
     }
